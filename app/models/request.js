62b428fa3c667f666e0fbb22bfe7b4ec75e807bb
Lamees AlFallaj <lafallaj2@gmail.com>
2019-12-14 13:11:30 +0300

    add the request model

---
+++ app/models/request.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose')
+const User = require('./user').User
+const statusSchema = new mongoose.Schema({
+  status: {type: String, required: true}
+})
+const Status = mongoose.model('Status', statusSchema)
+
+
+const reviewSchema = new mongoose.Schema({
+
+  user_id: {
+    type:mongoose.Schema.Types.ObjectId,
+    ref: User
+  },
+  title: {type: String, required:true},
+  content: {type: String, required:true},
+}, {timestamps: true,})
+  
+const requestSchema = new mongoose.Schema({
+
+  user_id: {
+    type:mongoose.Schema.Types.ObjectId,
+    ref: User
+  },
+  shop_name: {type: String, required:true},
+  shift: {type: String, required:true},
+  Days: [String],
+  details: {type: String, required:true},
+  status: {
+    type:mongoose.Schema.Types.ObjectId,
+    ref: Status
+  },
+  review: reviewSchema,
+}, {timestamps: true,})
+
+
+const Request = mongoose.model('Request', requestSchema)
+module.exports = {Request, Status}
